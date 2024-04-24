89bd7ca889da5f669287a76e944ef24e9d00f3bc
+ <+>
2024-04-24 12:51:29 +0300

    creates lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code here
+// Class for Breakfast
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Class for Lunch
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Class for Dinner
+  class Dinner {
+    #dessert; // Private property
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert;
+    }
+  }
+
+  const breakfast = new Breakfast("toast", "coffee");
+const lunch = new Lunch("green salad", "tomato soup", "iced tea");
+const dinner = new Dinner("Caesar salad", "butternut squash soup", "steak", "chocolate cake");
